907791a7665a8a50691490619a6a1d1782d5875c
Vladimir <shadrov_vladimir@mail.ru>
2023-07-14 16:16:42 +0300

    тест

--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 5500;
+const port = process.env.PORT || 5500;
 
 app.use(express.static(path.join(__dirname, '../')));
 
